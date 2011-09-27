3645a5392c63e22485556bc7df320de8465bb292
Jonanin <jonanin@gmail.com>
2011-09-26 20:00:39 -0500

    separate app and web stuff

--- chat/simplechat/app.js
+++ chat/simplechat/app.js
@@ -26,27 +26,8 @@
 
 var Class   = require("structr"),
     sio     = require("socket.io"),
-    express = require("express"),
-    _       = require("underscore");
-
-var app = express.createServer();
-
-// configure express to serve static files and use jade tempalting
-app.configure(function () {
-	app.use(express.static(__dirname + "/public"));
-	app.set("views", __dirname);
-	app.set("view engine", "jade");
-});
-
-// express stuff
-app.get("/", function (req, res) {
-	res.render("index", {layout: false});
-});
-
-app.listen(3000, function () {
-	var addr = app.address();
-	console.log("  app listening on " + addr.address + ":" + addr.port);
-});
+    _       = require("underscore"),
+    app     = require("./web.js");
 
 var io = sio.listen(app);
 
