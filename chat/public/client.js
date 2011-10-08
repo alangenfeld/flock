c6e350e712b2b5bf6e18f2c2e38113cde94f8b68
Jon Morton <jonanin@gmail.com>
2011-10-07 23:34:37 -0500

    standardize spacing

--- chat/public/client.js
+++ chat/public/client.js
@@ -3,9 +3,9 @@ var fbid_names = {};
 
 var Chat = {
     init : function() {
-      socket.on("msg", this.getMsg.bind(this));
-      $("#send").submit(this.sendMsg.bind(this));
-      Chat.uid = 0;
+        socket.on("msg", this.getMsg.bind(this));
+        $("#send").submit(this.sendMsg.bind(this));
+        Chat.uid = 0;
     },
 
     loggedIn : function(uid) {
@@ -16,67 +16,67 @@ var Chat = {
     },
     
     getMsg : function(data) {
-      var add = function(name, m) {
-        console.log("put " + name + m);
-        $("#text").append("<b>" + name + "</b>: " + m + "<br />");
-        $("#text").animate({ scrollTop: $("#text").prop("scrollHeight")});
-        $("#text").emoticonize({});
-      };
+        var add = function(name, m) {
+            console.log("put " + name + m);
+            $("#text").append("<b>" + name + "</b>: " + m + "<br />");
+            $("#text").animate({ scrollTop: $("#text").prop("scrollHeight")});
+            $("#text").emoticonize({});
+        };
 
-      var uid = data["userID"];
-      var _m = data["msg"];
-      console.log(_m);
-      if (!(uid in fbid_names)) {
-        getUserName(uid, function(name) {
-            fbid_names[uid] = name; 
-            add(name, _m);
-        });
-      } else {
-        add(fbid_names[uid], _m);
-      }
-      return false;
+        var uid = data["userID"];
+        var _m = data["msg"];
+        console.log(_m);
+        if (!(uid in fbid_names)) {
+            getUserName(uid, function(name) {
+                fbid_names[uid] = name; 
+                add(name, _m);
+            });
+        } else {
+            add(fbid_names[uid], _m);
+        }
+        return false;
     },
 
     sendMsg : function() {
-      socket.emit("msg", {"msg": $("#msg").val()});
-      $("#msg").val("");
-      return false;
+        socket.emit("msg", {"msg": $("#msg").val()});
+        $("#msg").val("");
+        return false;
     }
 };
 
 var Room = {
     init : function() {
-      var that = this;
-      socket.on("room_info", function(data) { 
-          that.updateRoomInfo(data);
-      });
-      this.pickContent(1, "Test");
+        var that = this;
+        socket.on("room_info", function(data) { 
+            that.updateRoomInfo(data);
+        });
+        this.pickContent(1, "Test");
     },
 
     updateRoomInfo : function(data) {
-      $("#roomName").text(data.room_name);
+        $("#roomName").text(data.room_name);
     },
 
     pickContent : function(cid, type) {
-      socket.emit("pick_content", {"contentID" : cid, "contentType" : type});
+        socket.emit("pick_content", {"contentID" : cid, "contentType" : type});
     }
 };
 
 $(document).ready(
-  function() {
-    socket = io.connect();
-    socket.on("connect", function() {});
-    Chat.init();
-    Room.init();
+    function() {
+        socket = io.connect();
+        socket.on("connect", function() {});
+        Chat.init();
+        Room.init();
 
-      //DEBUG
-      $("#testLogin").click(function(){
-	  	  Chat.loggedIn(0);
-	});
-  }
+        //DEBUG
+        $("#testLogin").click(function(){
+	  	    Chat.loggedIn(0);
+	    });
+    }
 );
 
 
 function chooseContent(cid, type) {
-  Room.pickContent(cid, type);
+    Room.pickContent(cid, type);
 }
