a74af8b0cdd4b740a418c15279d434e6775fe787
Adam Ecker <ecker2008@gmail.com>
2011-10-13 04:45:18 +0000

    added video categories

--- chat/public/video.js
+++ chat/public/video.js
@@ -3,40 +3,57 @@ $(document).ready(function()
    var category_query = 'http://api.justin.tv/api/category/list.json?jsonp=?';
    $.getJSON(category_query, function(categories)
    {
-//    $.each(categories, function(i,category)
-//    {
-         var channel_query = 'http://api.justin.tv/api/stream/list.json?jsonp=?';
-         args = { 'category': 'sports' };
-         $.getJSON(channel_query, args, function(channels)
-         {
-            var elSel = document.getElementById('selectVideo');
-            $.each(channels, function(j, channel)
-            {
-               var elOptNew = document.createElement('option');
-               elOptNew.text = channel.title;
-               elOptNew.value = channel.channel.login;
-               var elOptOld = elSel.options[elSel.selectedIndex];  
-               try {
-                  elSel.add(elOptNew, elSel.length);// standards compliant; doesn't work in IE
-               }
-               catch(ex) {
-                  elSel.add(elOptNew, elSel.selectedIndex); // IE only
-               }
-            });
-         });
-//    });
+       var elSel = document.getElementById('selectVideo');
+      // populate dropdown with categories to choose from
+      $.each(categories, function(i,category)
+      {
+        var elOptNew = document.createElement('option');
+        elOptNew.text = category.name;
+        elOptNew.value = i;
+        var elOptOld = elSel.options[elSel.selectedIndex];  
+        try {
+            elSel.add(elOptNew, elSel.length);// standards compliant; doesn't work in IE
+        }
+        catch(ex) {
+            elSel.add(elOptNew, elSel.selectedIndex); // IE only
+        }
+      });
    });
 });
 
 $("#selectVideo").change(function()
 {
-  var e = document.getElementById("selectVideo");
-  var val = e.options[e.selectedIndex].value;
-  var title = e.options[e.selectedIndex].text;
-  var html_code = "<div class='floatDiv' id='" + val + "'><object type=application/x-shockwave-flash height=295 width=353 data=http://www.justin.tv/widgets/jtv_player.swf?channel=" + val + " bgcolor=#000000><param name=allowFullScreen value=true /><param name=allowscriptaccess value=always /><param name=movie value=http://www.justin.tv/widgets/jtv_player.swf /><param name=flashvars value=channel=" + val + " bgcolor=#000000><param name=allowFullScreen value=true /><param name=allowscriptaccess value=always /><param name=movie value=http://www.justin.tv/widgets/jtv_player.swf /></object><br/><center><p><b>Title:</b> " + title.substring(0,35) + "<br/><a onmouseout=this.style.textDecoration='none' onmouseover=this.style.textDecoration='underline';this.style.cursor='pointer' onclick=removeStream('" + val + "')>Click to Remove Stream</a></center></p></div>";
-   chooseContent(val, 'justin.tv');
-  $("#video").append(html_code);
+    document.getElementById("video").innerHTML = "";
+    var e = document.getElementById("selectVideo");
+    var val = e.options[e.selectedIndex].value;
+    var channel_query = 'http://api.justin.tv/api/stream/list.json?jsonp=?';
+    var args = { 'category': val };
+    $.getJSON(channel_query, args, function(channels)
+    {
+//        listChannels(channels);
+        $.each(channels, function(j, channel)
+        {
+            if(channel.title)
+                {
+                    var elOptNew = document.createElement('a');
+                    elOptNew.id = channel.channel.login
+                    elOptNew.className = 'clickable';
+                    elOptNew.onclick = Function("displayVideo('"+elOptNew.id+"', '"+channel.title+"')");
+                    var elText = document.createTextNode(channel.title);
+                    elOptNew.appendChild(elText);
+                    $("#video").append(elOptNew);
+                    $("#video").append("<br/>");
+                }
+        });
+    });
 });
+
+function displayVideo(login, title)
+{
+    var html_code = "<div class='floatDiv' id='" + login + "'><object type=application/x-shockwave-flash height=295 width=353 data=http://www.justin.tv/widgets/jtv_player.swf?channel=" + login + " bgcolor=#000000><param name=allowFullScreen value=true /><param name=allowscriptaccess value=always /><param name=movie value=http://www.justin.tv/widgets/jtv_player.swf /><param name=flashvars value=channel=" + login + " bgcolor=#000000><param name=allowFullScreen value=true /><param name=allowscriptaccess value=always /><param name=movie value=http://www.justin.tv/widgets/jtv_player.swf /></object><br/><center><p><b>Title:</b> " + title.substring(0,35) + "<br/><a onmouseout=this.style.textDecoration='none' onmouseover=this.style.textDecoration='underline';this.style.cursor='pointer' onclick=removeStream('" + login + "')>Click to Remove Stream</a></center></p></div>";
+    chooseContent(login, 'justin.tv');
+    document.getElementById("video").innerHTML = html_code;
+}
 
 function removeStream(streamID)
 {
